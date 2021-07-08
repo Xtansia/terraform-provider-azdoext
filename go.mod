e19c2a7444135d50f481953bdbce8e69ea2fe359
Thomas Farr <xtansia@xtansia.com>
2021-07-08 16:41:36 +1200

    Tidy up

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/hashicorp/terraform-provider-scaffolding
+module github.com/Xtansia/terraform-provider-azdoext
 
 go 1.15
 
--- main.go
+++ main.go
@@ -6,26 +6,16 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/hashicorp/terraform-provider-scaffolding/internal/provider"
+
+	"github.com/Xtansia/terraform-provider-azdoext/internal/provider"
 )
 
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
-
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
-// ensure the documentation is formatted properly.
 //go:generate terraform fmt -recursive ./examples/
-
-// Run the docs generation tool, check its repository for more information on how it works and how docs
-// can be customized.
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 var (
-	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary
 	version string = "dev"
-
-	// goreleaser can also pass the specific commit if you want
-	// commit  string = ""
+	commit  string = ""
 )
 
 func main() {
@@ -37,8 +27,7 @@ func main() {
 	opts := &plugin.ServeOpts{ProviderFunc: provider.New(version)}
 
 	if debugMode {
-		// TODO: update this string with the full name of your provider as used in your configs
-		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/scaffolding", opts)
+		err := plugin.Debug(context.Background(), "registry.terraform.io/Xtansia/azdoext", opts)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
